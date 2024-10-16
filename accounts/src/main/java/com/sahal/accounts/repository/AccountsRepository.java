94bcb898eac20af081e95dbe6fdcad995d66f314
Sahal2000 <me.sahal2000@gmail.com>
2024-10-16 15:34:18 +0600

    commit 3

--- accounts/src/main/java/com/sahal/accounts/repository/AccountsRepository.java
+++ accounts/src/main/java/com/sahal/accounts/repository/AccountsRepository.java
@@ -11,8 +11,19 @@ import java.util.Optional;
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
 
+    /**
+     * Fetches the account for the customer with the given customer id.
+     *
+     * @param customerId the customer id to fetch the account for
+     * @return an optional containing the account for the given customer id
+     */
     Optional<Accounts> findByCustomerId(Long customerId);
 
+    /**
+     * Deletes the account for the customer with the given customer id.
+     *
+     * @param customerId the customer id to delete the account for
+     */
     @Transactional
     @Modifying
     void deleteByCustomerId(Long customerId);
