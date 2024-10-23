4c712da799b6fd2b97cd33cdff71ddc5505a6149
Sahal2000 <me.sahal2000@gmail.com>
2024-10-23 19:34:38 +0600

    commit 2

--- accounts/src/main/java/com/sahal/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/sahal/accounts/AccountsApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AccountsApplication {
 
+	/**
+	 * Entry point of the Spring Boot application.
+	 *
+	 * @param args Command line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
 	}
