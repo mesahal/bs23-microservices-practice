ae9c3e47dc07d144bd2f9a2c90d9c62c544d2e5b
Sahal2000 <me.sahal2000@gmail.com>
2024-10-23 19:33:00 +0600

    commit 1

--- accounts/src/main/java/com/sahal/accounts/mapper/CustomerMapper.java
+++ accounts/src/main/java/com/sahal/accounts/mapper/CustomerMapper.java
@@ -4,6 +4,14 @@ import com.sahal.accounts.dto.CustomerDto;
 import com.sahal.accounts.entity.Customer;
 
 public class CustomerMapper {
+    /**
+     * Maps the given {@link Customer} entity to the given {@link CustomerDto} and
+     * returns the mapped {@link CustomerDto}.
+     *
+     * @param customer the {@link Customer} entity to map from
+     * @param customerDto the {@link CustomerDto} to map to
+     * @return the mapped {@link CustomerDto}
+     */
     public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
         customerDto.setName(customer.getName());
         customerDto.setEmail(customer.getEmail());
@@ -11,6 +19,13 @@ public class CustomerMapper {
         return customerDto;
     }
 
+    /**
+     * Maps the given {@link CustomerDto} to the given {@link Customer} entity.
+     *
+     * @param customerDto the {@link CustomerDto} to map from
+     * @param customer the {@link Customer} entity to map to
+     * @return the mapped {@link Customer} entity
+     */
     public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
         customer.setName(customerDto.getName());
         customer.setEmail(customerDto.getEmail());
--- accounts/src/main/java/com/sahal/accounts/repository/CustomerRepository.java
+++ accounts/src/main/java/com/sahal/accounts/repository/CustomerRepository.java
@@ -9,6 +9,12 @@ import java.util.Optional;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
+    /**
+     * Fetches the customer details for the given mobile number.
+     *
+     * @param mobileNumber the mobile number to fetch the customer details for
+     * @return an optional containing the customer details if found, otherwise an empty optional
+     */
     Optional<Customer> findByMobileNumber(String mobileNumber);
 
 
