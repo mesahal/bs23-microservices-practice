c9affd4a8d28998be229b050f1087597661fb7f1
Sahal2000 <me.sahal2000@gmail.com>
2024-10-28 19:22:42 +0600

    commit 4

--- cards/src/main/java/com/sahal/cards/dto/CardsDto.java
+++ cards/src/main/java/com/sahal/cards/dto/CardsDto.java
@@ -1,4 +1,11 @@
 package com.sahal.cards.dto;
 
 public class CardsDto {
+    private Long cardId;
+    private String mobileNumber;
+    private String cardNumber;
+    private String cardType;
+    private int totalLimit;
+    private int amountUsed;
+    private int availableAmount;
 }
--- cards/src/main/java/com/sahal/cards/entity/BaseEntity.java
+++ cards/src/main/java/com/sahal/cards/entity/BaseEntity.java
@@ -8,9 +8,11 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Data
 public class BaseEntity {
