a28cecc2cbbefcd6e1c23a47e793d7fe2a4619e7
Sahal2000 <me.sahal2000@gmail.com>
2024-10-29 19:24:18 +0600

    commit 2

--- cards/src/main/java/com/sahal/cards/dto/ErrorResponseDto.java
+++ cards/src/main/java/com/sahal/cards/dto/ErrorResponseDto.java
@@ -1,8 +1,16 @@
 package com.sahal.cards.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
+
+@Schema(
+        name = "ErrorResponse",
+        description = "Schema to hold error response information"
+)
 @Data
 @AllArgsConstructor
 public class ErrorResponseDto {
