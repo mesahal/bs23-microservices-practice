58194484316679510eb0dbb9b451e31b892a5fc9
Sahal2000 <me.sahal2000@gmail.com>
2024-10-29 19:21:02 +0600

    commit 1

--- cards/src/main/java/com/sahal/cards/dto/ErrorResponseDto.java
+++ cards/src/main/java/com/sahal/cards/dto/ErrorResponseDto.java
@@ -1,4 +1,17 @@
 package com.sahal.cards.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ErrorResponseDto {
+
+    private String apiPath;
+
+    private HttpStatus errorCode;
+
+    private String errorMessage;
+
+    private LocalDateTime errorTime;
 }
