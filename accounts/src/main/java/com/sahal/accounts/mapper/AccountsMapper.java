b02ce94f4b8c216d2ebbc3a06acfcd5cfd59c838
Sahal2000 <me.sahal2000@gmail.com>
2024-11-01 10:52:56 +0600

    commit 4

--- accounts/src/main/java/com/sahal/accounts/mapper/AccountsMapper.java
+++ accounts/src/main/java/com/sahal/accounts/mapper/AccountsMapper.java
@@ -4,14 +4,7 @@ import com.sahal.accounts.dto.AccountsDto;
 import com.sahal.accounts.entity.Accounts;
 
 public class AccountsMapper {
-    /**
-     * Maps the given {@link Accounts} entity to the given {@link AccountsDto} and
-     * returns the mapped {@link AccountsDto}.
-     *
-     * @param accounts the {@link Accounts} entity to map from
-     * @param accountsDto the {@link AccountsDto} to map to
-     * @return the mapped {@link AccountsDto}
-     */
+
     public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
         accountsDto.setAccountNumber(accounts.getAccountNumber());
         accountsDto.setAccountType(accounts.getAccountType());
@@ -19,12 +12,7 @@ public class AccountsMapper {
         return accountsDto;
     }
 
-    /**
-     * Maps an {@link AccountsDto} to an {@link Accounts} instance.
-     * @param accountsDto the source
-     * @param accounts the target
-     * @return the target with its fields set to the values of the source
-     */
+
     public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
         accounts.setAccountNumber(accountsDto.getAccountNumber());
         accounts.setAccountType(accountsDto.getAccountType());
