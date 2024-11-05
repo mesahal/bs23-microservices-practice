708667248fcead7441008fc12684892b477d8263
Sahal2000 <me.sahal2000@gmail.com>
2024-11-05 12:37:10 +0600

    commit 2

--- cards/src/main/java/com/sahal/cards/controller/CardsController.java
+++ cards/src/main/java/com/sahal/cards/controller/CardsController.java
@@ -2,6 +2,8 @@ package com.sahal.cards.controller;
 
 import com.sahal.cards.dto.ResponseDto;
 import com.sahal.cards.service.ICardsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+        name = "CRUD REST APIs for Cards in EazyBank",
+        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE card details"
+)
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
@@ -16,6 +22,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CardsController {
     private ICardsService iCardsService;
 
+    @Operation(
+        summary = "Create Card REST API",
+        description = "REST API to create new card inside EazyBank"
+    )
     public ResponseEntity<ResponseDto> createCard() {
         return null;
     }
