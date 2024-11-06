88bc25e429dcc3fcf22b0643439b2f7a593629f0
Sahal2000 <me.sahal2000@gmail.com>
2024-11-06 17:53:29 +0600

    commit 2

--- accounts/src/main/java/com/sahal/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/sahal/accounts/controller/AccountsController.java
@@ -13,7 +13,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -28,12 +30,16 @@ public class AccountsController {
 
     private final IAccountsService iAccountsService;
 
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Autowired
+    private Environment environment;
+
+
     public AccountsController(IAccountsService iAccountsService) {
         this.iAccountsService = iAccountsService;
     }
-
-    @Value("${build.version}")
-    private String buildVersion;
 
     @Operation(
             summary = "Create Account REST API",
@@ -191,5 +197,31 @@ public class AccountsController {
                 .body(buildVersion);
     }
 
+
+
+    @Operation(
+            summary = "Get Java Version",
+            description = "Get java version that is installed into account microservice"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("java.home"));
+    }
 
 }
